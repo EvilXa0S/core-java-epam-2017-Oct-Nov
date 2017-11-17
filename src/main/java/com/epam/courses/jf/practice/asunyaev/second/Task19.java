197cc63c661621767fbfa3681cb251c229fdbc12
asunyaev <a.a.syunyaev@gmail.com>
2017-11-17 19:41:53 +0300

    Task19 javadoc added

--- src/main/java/com/epam/courses/jf/practice/asunyaev/second/Task19.java
+++ src/main/java/com/epam/courses/jf/practice/asunyaev/second/Task19.java
@@ -4,8 +4,21 @@ import com.epam.courses.jf.practice.common.second.ITestableTask19;
 
 import java.util.*;
 
+/**
+ * Реализация интерфейса для юнит-тестирования задания №19.
+ *
+ * На кольцевой гоночной трассе стоит N автомобилей.
+ * Для каждого из них известны начальное положение и скорость.
+ * Определить, сколько произойдет обгонов за указанное количество кругов.
+ */
 public class Task19 implements ITestableTask19 {
 
+    /**
+     * @param cars Расположенные на трассе машины.
+     * @param lengthLap Длина трассы.
+     * @param numberLaps Количество кругов.
+     * @return Количество осуществленных обгонов.
+     */
     @Override
     public int getNumberOvertaking(Set<ICar> cars, long lengthLap, int numberLaps) {
         int numberOvertaking = 0;
