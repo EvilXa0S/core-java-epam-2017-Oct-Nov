94d2325a47a4c9a1297f600696b4a0c8199ff28e
BorzdovBogdan <bodja7.borzdov@yandex.ru>
2017-11-20 12:56:24 +0300

    task 2.11
    (cherry picked from commit 47af521)

--- src/main/java/com/epam/courses/jf/practice/bborzdov/Task11.java
+++ src/main/java/com/epam/courses/jf/practice/bborzdov/Task11.java
@@ -4,6 +4,7 @@ import com.epam.courses.jf.practice.common.second.ITestableTask11;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -15,22 +16,41 @@ public class Task11 implements ITestableTask11 {
         int step=0;
         int peoplesSize = peoples.size()-1;
         peoples.set(0,"");
-            for (int i = 0; peoplesSize > 1 ; i++) {
-                if(!peoples.get(i % peoples.size()).equals("")){
-                    step++;
-                    if(step==2){
-                        peoples.set(i % peoples.size(), "");
-                        peoplesSize--;
-                        step=0;
-                    }
+        for (int i = 0; peoplesSize > 1 ; i++) {
+            if(!peoples.get(i % peoples.size()).equals("")){
+                step++;
+                if(step==2){
+                    peoples.set(i % peoples.size(), "");
+                    peoplesSize--;
+                    step=0;
                 }
             }
-            peoples.removeAll(Collections.singleton(""));
+        }
+        peoples.removeAll(Collections.singleton(""));
         return peoples.get(0);
     }
 
     @Override
     public String emulate(LinkedList<String> peoples) {
-        return null;
+        peoples.removeFirst();
+        Iterator<String> it1 = peoples.listIterator();
+        int step = 1;
+        it1.next();
+        while (peoples.size() > 1){
+            if(!it1.hasNext()){
+                if(peoples.size() == 1){
+                    return peoples.getFirst();
+                }
+                it1 = peoples.listIterator();
+            }else {
+                it1.next();
+                step++;
+                if(step == 2) {
+                    it1.remove();
+                    step = 0;
+                }
+            }
+        }
+        return peoples.getFirst();
     }
 }
