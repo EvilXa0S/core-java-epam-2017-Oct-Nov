082c8c0092a6bd3af803e98fd124334afb778739
dana <danilikus17@gmail.com>
2017-11-23 00:23:46 +0300

    19

--- src/main/java/com/epam/courses/jf/practice/danilBay/second/Task2.java
+++ src/main/java/com/epam/courses/jf/practice/danilBay/second/Task2.java
@@ -11,28 +11,22 @@ public class Task2 implements ITestableTask2 {
     static private File[]deepDarkFantasy(File[] fileArrray){
 
             for (File x: fileArrray) {
+                fileSet.add(x);
                 if(!(x.listFiles() ==null)){
-                    fileSet.add(x);
+
                     deepDarkFantasy(x.listFiles());
-                }
-                else {
-                    fileSet.add(x);
                 }
             }
         return null;
     }
     @Override
     public Set<File> getFiles(File directory) {
-        File[] start={directory};
-        deepDarkFantasy(start);
-
-        fileSet.remove(directory);
-
+        deepDarkFantasy(directory.listFiles());
         return fileSet;
     }
 
     public static void main(String[] args) {
         Task2 m=new Task2();
-        m.getFiles(new File("C:\\xampp\\htdocs\\jobPhp\\adminV"));
+        m.getFiles(new File("C:\\xampp"));
     }
 }
