eb80c1990edc24a2e550ae70e7f168b037175d1b
vakhonin <isvakhonin@yandex.ru>
2017-11-22 18:14:32 +0300

    add_only_task2_16

--- src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task16.java
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task16.java
@@ -1,171 +1,3 @@
-//package com.epam.courses.jf.practice.vakhonin.second;
-//
-//import com.epam.courses.jf.practice.common.second.I2DPoint;
-//import com.epam.courses.jf.practice.common.second.ITestableTask16;
-//
-//import java.io.*;
-//import java.util.Comparator;
-//import java.util.Map;
-//import java.util.SortedMap;
-//import java.util.TreeMap;
-//
-///**
-// * Created by igorvahonin on 13.11.17.
-// */
-//public class Task16 implements ITestableTask16{
-//
-//    class Point implements I2DPoint{
-//        private double x, y;
-//
-//        Point(double x, double y){
-//            this.x = x;
-//            this.y = y;
-//        }
-//
-//        public void setX(double x) {
-//            this.x = x;
-//        }
-//
-//        public void setY(double y) {
-//            this.y = y;
-//        }
-//
-//        public double getX() {
-//            return x;
-//        }
-//
-//        public double getY() {
-//            return y;
-//        }
-//
-//
-//
-//    }
-//
-//    public double distance(I2DPoint point1, I2DPoint point2){
-//        double result;
-//        result = Math.sqrt(Math.pow(point1.getX() - point2.getX(), 2) + Math.pow(point1.getY() - point2.getY(), 2));
-//        return result;
-//    }
-//
-//    public boolean isPointInCircle(I2DPoint point, I2DPoint center, int radius){
-//
-//        double distance = distance(point, center);
-//
-//        if(distance < Double.valueOf(radius)){
-//            return true;
-//        }
-//        else {
-//            return false;
-//        }
-//
-//    }
-//
-//    @Override
-//    public IFileWithPoints analyze(I2DPoint center, int radius, File output) {
-//        int integerCenterX = (int) center.getX();
-//        int integerCenterY = (int) center.getY();
-//        int j = integerCenterX;
-//        int k = integerCenterY;
-//
-//        Comparator<I2DPoint> comparator = new Comparator<I2DPoint>() {
-//            @Override
-//            public int compare(I2DPoint p1, I2DPoint p2) {
-//                double r1 = distance(p1, center);
-//                double r2 = distance(p2, center);
-//                return Double.compare(r1, r2);
-//            }
-//        };
-//
-//
-//        SortedMap<I2DPoint, Double> map = new TreeMap<>(comparator);
-//
-//        Point point = new Point(j,k);
-//
-//        while(isPointInCircle(point, center, radius)){
-//            while(isPointInCircle(point, center, radius)){
-//                map.put(point, distance(point, center));
-//                point.setX(point.getX()+1);
-//            }
-//            point.setX(integerCenterX);
-//            while(isPointInCircle(point, center, radius)){
-//                map.put(point, distance(point, center));
-//                point.setX(point.getX()-1);
-//            }
-//            point.setY(point.getY()+1);
-//            point.setX(integerCenterX);
-//        }
-//
-//        while(isPointInCircle(point, center, radius)){
-//            while(isPointInCircle(point, center, radius)){
-//                map.put(point, distance(point, center));
-//                point.setX(point.getX()+1);
-//            }
-//            point.setY(integerCenterY);
-//            while(isPointInCircle(point, center, radius)){
-//                map.put(point, distance(point, center));
-//                point.setX(point.getX()-1);
-//            }
-//            point.setX(integerCenterX);
-//            point.setY(point.getY()-1);
-//        }
-//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(output))) {
-//            if(!map.isEmpty()) {
-//                for(I2DPoint p: map.keySet()) {
-//                    writer.write("" + p.getX());
-//                    writer.write(" ");
-//                    writer.write("" + p.getY());
-//                    writer.write(" ");
-//                    writer.write("" + map.get(p));
-//                    writer.write("\n");
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return new FileWithPoints(output, comparator);
-//    }
-//
-//
-//    public static class FileWithPoints implements IFileWithPoints{
-//        private final File file;
-//        private SortedMap<I2DPoint, Double> points;
-//        private Comparator<I2DPoint> comparator;
-//
-//        FileWithPoints(File file, Comparator<I2DPoint> comparator) {
-//            this.file = file;
-//            this.comparator = comparator;
-//        }
-//
-//        public File getFile(){
-//            return file;
-//        }
-//
-//        public SortedMap<I2DPoint, Double> getPoints(){
-//            if(points == null){
-//                points = new TreeMap<>(comparator);
-//                try(FileInputStream fileInputStream = new FileInputStream(file);
-//                    ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)){
-//                    Map.Entry<I2DPoint, Double> entry;
-//                    while ((entry = (Map.Entry<I2DPoint, Double>) objectInputStream.readObject()) != null){
-//                        points.put(entry.getKey(), entry.getValue());
-//                    }
-//                } catch (IOException | ClassNotFoundException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            return points;
-//        }
-//    }
-//
-//}
-
-
-//
-
-
-
 package com.epam.courses.jf.practice.vakhonin.second;
 
 import com.epam.courses.jf.practice.common.second.I2DPoint;
@@ -194,16 +26,7 @@ public class Task16 implements ITestableTask16 {
         public double getY() {
             return y;
         }
-
-        void setX(double x){
-            this.x = x;
-        }
-
-        void setY(double y){
-            this.y = y;
-        }
     }
-
 
     public double calculateDistance(I2DPoint point1, I2DPoint point2){
         double dist = Math.sqrt(Math.pow(point1.getX() - point2.getX(), 2) + Math.pow(point1.getY() - point2.getY(), 2));
@@ -225,18 +48,14 @@ public class Task16 implements ITestableTask16 {
     }
 //    static Logger log = Logger.getLogger(Task16.class.getName());
 
-
-    String mapString(Map<I2DPoint, Double> map){
-        String result = "MAP_STRING: ";
-        for (Map.Entry<I2DPoint, Double> entry : map.entrySet()) {
-            result += "x = " + entry.getKey().getX() + "  y = " + entry.getKey().getY() + "\n";
-        }
-        return result;
+    String stringOfPointForFile(I2DPoint point, I2DPoint center){
+        String str = String.valueOf(point.getX()) + " " + String.valueOf(point.getY()) + " " + String.valueOf(calculateDistance(point, center) + "\n");
+        return str;
     }
+
 
     @Override
     public IFileWithPoints analyze(I2DPoint center, int radius, File output) {
-
 
         double xCenter = (int) center.getX();
         double yCenter = (int) center.getY();
@@ -253,7 +72,6 @@ public class Task16 implements ITestableTask16 {
             }
         };
 
-//        Queue<I2DPoint> queue = new PriorityQueue<>(comparator);
         SortedMap<I2DPoint, Double> map = new TreeMap<>(comparator);
 
         I2DPoint point;
@@ -266,9 +84,8 @@ public class Task16 implements ITestableTask16 {
         for (int j = xFirst; j <= xEnd; j++) {
             for (int k = yFirst; k <= yEnd; k++) {
                 point = new Point2D(j, k);
-                if (calculateDistance(point, center) < radius) {
+                if (isPointInCircle(point, center, radius)) {
                     map.put(point, calculateDistance(point, center));
-//                    queue.offer(point);
                 }
             }
         }
@@ -276,31 +93,24 @@ public class Task16 implements ITestableTask16 {
 
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(output))) {
+
+
+
             writer.write(String.valueOf(center.getX()));
             writer.write(" ");
             writer.write(String.valueOf(center.getY()));
             writer.write("\n");
 
+            for (Map.Entry<I2DPoint, Double> entry : map.entrySet()) {
+                point = entry.getKey();
+                writer.write(stringOfPointForFile(point, center));
 
-
-//            while(! queue.isEmpty()){
-//                point = queue.poll();
 //                writer.write(String.valueOf(point.getX()));
 //                writer.write(" ");
 //                writer.write(String.valueOf(point.getY()));
 //                writer.write(" ");
-//                writer.write(String.valueOf(dist(point, center)));
+//                writer.write(String.valueOf(calculateDistance(point, center)));
 //                writer.write("\n");
-//            }
-
-            for (Map.Entry<I2DPoint, Double> entry : map.entrySet()) {
-                point = entry.getKey();
-                writer.write(String.valueOf(point.getX()));
-                writer.write(" ");
-                writer.write(String.valueOf(point.getY()));
-                writer.write(" ");
-                writer.write(String.valueOf(calculateDistance(point, center)));
-                writer.write("\n");
             }
 
         } catch (IOException e) {
