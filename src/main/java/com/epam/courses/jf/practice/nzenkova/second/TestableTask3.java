dc789f3f2d4cb60947b60ca99d9d3b44c69ba2b6
nzenkova <natalka22-96@hotmail.com>
2017-11-24 20:39:11 +0300

    Task2 -- modernization 3

--- src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask3.java
+++ src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask3.java
@@ -7,11 +7,21 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * The strings, which make up the poems, write to a list.
+ * Sort strings in ascending order of their length.
+ */
 
 public class TestableTask3 implements ITestableTask3 {
     @Override
+    /**
+     * Forms an ordered list of lines from the poems of given author.
+     * @param poems The list of the assayed  poems.
+     * @param author The author whose poems will be chosen.
+     * @return An ordered list of lines, which make up the poems of given author.
+     */
     public List<String> sortPoems(Set<IPoem> poems, String author) {
-        ArrayList<String> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         for (IPoem poem : poems) {
             if (poem.getAuthor().equals(author)) {
                 list.addAll(poem.getLines());
