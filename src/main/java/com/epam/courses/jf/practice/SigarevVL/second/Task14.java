9331f58f6d6e67aab851662b9fd46ff4450c018f
SigarevVL <SigarevVL94@gmail.com>
2017-11-25 02:09:06 +0300

    14 task repair

--- src/main/java/com/epam/courses/jf/practice/SigarevVL/second/Task14.java
+++ src/main/java/com/epam/courses/jf/practice/SigarevVL/second/Task14.java
@@ -4,6 +4,8 @@ import com.epam.courses.jf.practice.common.second.ITestableTask14;
 
 import java.util.*;
 
+import static java.lang.Math.abs;
+
 /**
  * Интерфейс для юнит-тестирования задания №14.
  *
@@ -29,18 +31,53 @@ public class Task14 implements ITestableTask14 {
     private class INumberCollectionImpl <T extends Number> extends ArrayList<T> implements  INumberCollection<T> {
 
         List<T> list = new ArrayList<>();
+//        @Override
+//        public T nearest(Number value) {
+//            double subMod = Math.abs(list.get(0).doubleValue() - value.doubleValue());
+//            T minimal = list.get(0);
+//            Iterator<T> iterator = list.iterator();
+//            while (iterator.hasNext()) {
+//                if (Math.abs(iterator.next().doubleValue() - value.doubleValue()) < subMod) {
+//                    minimal = iterator.next();
+//                }
+//            }
+//            return minimal ;
+//
+//        }
+
         @Override
-        public T nearest(Number value) {
-            double subMod = Math.abs(list.get(0).doubleValue() - value.doubleValue());
-            T minimal = list.get(0);
-            Iterator<T> iterator = list.iterator();
-            while (iterator.hasNext()) {
-                if (Math.abs(iterator.next().doubleValue() - value.doubleValue()) < subMod) {
-                    minimal = iterator.next();
+        public T nearest(T value) {
+            if (list.isEmpty()) {
+                return null;
+            }
+
+            T nearest = list.get(0);
+            double doubleValue = value.doubleValue();
+            double minDifference = abs(nearest.doubleValue());
+
+            for (T t : list) {
+                if (abs(doubleValue - t.doubleValue()) < minDifference) {
+                    minDifference = abs(doubleValue - t.doubleValue());
+                    nearest = t;
                 }
             }
-            return minimal ;
 
+            return nearest;
+        }
+
+        @Override
+        public int size() {
+            return list.size();
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            return list.iterator();
+        }
+
+        @Override
+        public boolean add(T t) {
+            return list.add(t);
         }
     }
 }
