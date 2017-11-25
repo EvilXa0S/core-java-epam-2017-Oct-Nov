1ce99cc4b1cbe1e6dd467a7dce223f8f295c5255
nzenkova <natalka22-96@hotmail.com>
2017-11-25 14:18:35 +0300

    Task2 -- modification 9

--- src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask9.java
+++ src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask9.java
@@ -9,8 +9,18 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 
-
+/**
+ * Specify a file, which contains english words (without punctuation marks).
+ * Find all different words.
+ * Words that differ only in the case of letters are assumed to be the same.
+ * Use HashSet.
+ */
 public class TestableTask9 implements ITestableTask9 {
+    /**
+     * Form a set of unique words, which are extracted from a file.
+     * @param input The file being analyzed.
+     * @return A set of the resultant words.
+     */
     @Override
     public HashSet<String> getUniqueWords(File input) {
         HashSet<String> strings = new HashSet<>();
