19b8627be0baa5594f004ac225a5f2f11b374ee2
AleksandrKriukov <alexanderkry@gmail.com>
2017-11-25 11:51:11 +0300

    Task2.2 done
    TaskStorage created

---
+++ src/main/java/com/epam/courses/jf/practice/akriukov/second/Task2.java
@@ -0,0 +1,26 @@
+package com.epam.courses.jf.practice.akriukov.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask2;
+
+import java.io.File;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Task2 implements ITestableTask2 {
+    @Override
+    public Set<File> getFiles(File directory) {
+        Set<File> set = new TreeSet<>();
+        return getFiles(directory, set);
+    }
+
+    private Set<File> getFiles(File directory, Set<File> set) {
+        for(File file: directory.listFiles()) {
+            if(file.isDirectory()) {
+                set.add(file);
+                getFiles(file, set);
+            } else
+                set.add(file);
+        }
+        return set;
+    }
+}
