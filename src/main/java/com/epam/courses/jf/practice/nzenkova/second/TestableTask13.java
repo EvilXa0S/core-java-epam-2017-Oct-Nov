07f4ee2dbdb83cca062b5c690cf49c9f9f9968f8
nzenkova <natalka22-96@hotmail.com>
2017-11-25 22:40:31 +0300

    Task2 -- 13

--- src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask13.java
+++ src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask13.java
@@ -2,20 +2,16 @@ package com.epam.courses.jf.practice.nzenkova.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask13;
 
-/**
- * Realize the class Graph, which is an undirected graph(there is only one arc between two vertices).
- * The constructor of the class takes the amount of the vertices in the graph.
- * The methods should support the quick addition and removal of the arcs.
- */
+
 public class TestableTask13 implements ITestableTask13 {
-        /**
-         * @param numberNodes The amount of vertices in the graph.
-         * @return The graph of the specified configuration.
-         */
-        @Override
-        public AbstractGraph createGraph(int numberNodes) {
-            return new Graph(numberNodes);
-        }
+    /**
+     * @param numberNodes The amount of vertices in the graph.
+     * @return The graph of the specified configuration.
+     */
+    @Override
+    public AbstractGraph createGraph(int numberNodes) {
+        return new Graph(numberNodes);
+    }
 
     class Graph extends AbstractGraph {
         private int[][] graph;
@@ -37,7 +33,6 @@ public class TestableTask13 implements ITestableTask13 {
             graph[first][second] = 0;
         }
 
-        
         @Override
         public boolean isExistEdge(int first, int second) {
             return graph[first][second] == 1;
@@ -45,5 +40,3 @@ public class TestableTask13 implements ITestableTask13 {
     }
 
 }
-
-
