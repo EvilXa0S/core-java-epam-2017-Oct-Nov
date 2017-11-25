b17e516901920e38b7cdd6fcd25f490a4e8d4bcd
nzenkova <natalka22-96@hotmail.com>
2017-11-25 12:29:11 +0300

    Task2 -- modification 6

--- src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask6.java
+++ src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask6.java
@@ -5,7 +5,18 @@ import com.epam.courses.jf.practice.common.second.ITestableTask6;
 import java.util.HashMap;
 
 
+/**
+ * Add two polynomials, if the coefficients of the polynomials are kept in HashMap.
+ * A degree of the element of the polynomial is a key, a coefficient of the element of the polynomial is a value.
+ * The coefficients of the polynomials, whose keys aren't be in a map, are equal to zero.
+ */
 public class TestableTask6 implements ITestableTask6 {
+    /**
+     * Add two polynomials.
+     * @param first The first polynomial.
+     * @param second The second polynomial.
+     * @return Sum of two polynomials.
+     */
     @Override
     public HashMap<Integer, Integer> addPolynomials(HashMap<Integer, Integer> first, HashMap<Integer, Integer> second) {
         for (Integer integer : second.keySet()) {
@@ -14,7 +25,6 @@ public class TestableTask6 implements ITestableTask6 {
             if (second.get(integer) != null) toPut += second.get(integer);
             first.put(integer, toPut);
         }
-
         return first;
     }
 }
