d30aaa483c2abe72d753aaaca7a143ff3f2af801
nzenkova <natalka22-96@hotmail.com>
2017-11-25 03:24:54 +0300

    Task2 -- modernization 5

--- src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask5.java
+++ src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask5.java
@@ -5,8 +5,18 @@ import com.epam.courses.jf.practice.common.second.ITestableTask5;
 import java.math.BigDecimal;
 import java.util.List;
 
-
+/**
+ * The list contains the result of current and voltage measurements on the unknown resistance R.
+ * Find the approximate number R by the method of least squares.
+ * Use the class {@link java.math.BigDecimal} for increasing accuracy of the calculation.
+ * The accuracy is not less than 1e-6. Guarantee it.
+ */
 public class TestableTask5 implements ITestableTask5 {
+    /**
+     * Calculate the resistance with the method of least squares.
+     * @param measurements The measurements in the experiment.
+     * @return The resistance is calculated from the initial data.
+     */
     @Override
     public double calcResistance(List<IMeasurement> measurements) {
         BigDecimal sum1 = new BigDecimal(0d);
