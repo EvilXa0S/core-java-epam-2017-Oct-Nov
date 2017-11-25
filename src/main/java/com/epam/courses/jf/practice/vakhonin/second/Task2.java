f161a8dcb11fd0420909de0e48f93b1f04cb2024
vakhonin <isvakhonin@yandex.ru>
2017-11-25 22:43:04 +0300

    add_only_task2_18

--- src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task2.java
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task2.java
@@ -1,7 +1,29 @@
 package com.epam.courses.jf.practice.vakhonin.second;
 
+import com.epam.courses.jf.practice.common.second.ITestableTask2;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by igorvahonin on 13.11.17.
  */
-public class Task2 {
+public class Task2 implements ITestableTask2 {
+
+    Set<File> allFiles = new HashSet<>();
+
+    @Override
+    public Set<File> getFiles(File directory) {
+
+        for(File f: directory.listFiles()){
+            if(f.isDirectory()){
+                allFiles.addAll(getFiles(f));
+            }
+            else{
+                allFiles.add(f);
+            }
+        }
+        return allFiles;
+    }
 }
