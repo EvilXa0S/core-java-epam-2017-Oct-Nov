0121fd394488c3c9141a8a7480a84368d2c25673
nzenkova <natalka22-96@hotmail.com>
2017-11-25 14:03:18 +0300

    Task2 -- modification 8

--- src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask8.java
+++ src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask8.java
@@ -6,7 +6,21 @@ import com.epam.courses.jf.practice.common.second.ITestableTask8;
 import java.util.HashMap;
 import java.util.Stack;
 
+
+/**
+ * Specify a string. The string can contain '(', ')', '[', ']', '{', '}'.
+ * Check balanced brackets.
+ * Use a stack.
+ */
 public class TestableTask8 implements ITestableTask8 {
+    /**
+     * Check balanced brackets.
+     * Correct an arrangement:
+     *                          1) Each opening parenthesis corresponds to a closing of the same type.
+     *                          2) There is no intersection of bracketed areas.
+     * @param string The string being analyzed.
+     * @return true -- Brackets are balanced, false -- otherwise.
+     */
     @Override
     public boolean isNormalBrackets(String string) {
         HashMap<Character, Character> map = new HashMap<>();
