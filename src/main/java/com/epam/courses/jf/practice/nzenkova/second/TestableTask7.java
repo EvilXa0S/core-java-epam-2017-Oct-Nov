6fd4ee7da4de4f8ebe01e3ae4f7455117892974f
nzenkova <natalka22-96@hotmail.com>
2017-11-25 12:28:51 +0300

    Task2 -- modification 7

--- src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask7.java
+++ src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask7.java
@@ -5,8 +5,19 @@ import com.epam.courses.jf.practice.common.second.ITestableTask7;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Multiply two polynomials of the default degree.
+ * The element of the list with the index i corresponds to the coefficient of the degree i.
+ * The degree of the resultant polynomials is equal to the sum of the maximum nonzero degrees of the initial polynomials.
+ * If the polynomial degenerates in zero, the state is [0].
+ */
 public class TestableTask7 implements ITestableTask7 {
+    /**
+     * Multiply two polynomials.
+     * @param first The first polynomial.
+     * @param second The second polynomial.
+     * @return The product of two polynomials.
+     */
     @Override
     public List<Integer> multiplyPolynomials(List<Integer> first, List<Integer> second) {
         int size = first.size() + second.size();
