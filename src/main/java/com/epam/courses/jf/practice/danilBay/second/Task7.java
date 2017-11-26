dbf3b860944c5bc2f91fa6cc712e15ad5a379a08
dana <danilikus17@gmail.com>
2017-11-26 19:43:24 +0300

    19

--- src/main/java/com/epam/courses/jf/practice/danilBay/second/Task7.java
+++ src/main/java/com/epam/courses/jf/practice/danilBay/second/Task7.java
@@ -8,12 +8,15 @@ import java.util.List;
 public class Task7 implements ITestableTask7 {
     @Override
     public List<Integer> multiplyPolynomials(List<Integer> first, List<Integer> second) {
-        List<Integer> result=new ArrayList<>(first.size()*second.size());
+        List<Integer> result=new ArrayList<>();
+        for(int i=0; i<first.size()*second.size();i++){
+            result.add(0);
+        }
         int buf;
         for(int i=0; i< first.size();i++)
             for(int j=0; j< second.size();j++){
                 buf = result.get(i*j);
-                result.set(i*j,buf + first.get(i)*second.get(j));
+                result.add(i*j,buf + first.get(i)*second.get(j));
             }
         return result;
     }
