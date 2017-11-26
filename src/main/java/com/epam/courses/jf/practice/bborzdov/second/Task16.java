61117af3485c508dd9338380935b35e08380bff8
BorzdovBogdan <bodja7.borzdov@yandex.ru>
2017-11-27 02:53:08 +0300

    2.16 central point addition

--- src/main/java/com/epam/courses/jf/practice/bborzdov/second/Task16.java
+++ src/main/java/com/epam/courses/jf/practice/bborzdov/second/Task16.java
@@ -29,17 +29,30 @@ public class Task16 implements ITestableTask16 {
     @Override
     public IFileWithPoints analyze(I2DPoint center, int radius, File output) {
         FileWithPoints file = new FileWithPoints(output);
-        double radiusDouble = (double) radius / 2;
+        float floatRadius = (float) radius / 2;
         SortedMap<I2DPoint, Double> points = new TreeMap<>();
-        for (int i = (int)(center.getY() + radiusDouble); i > (center.getY() - radiusDouble - 1) ; i--) {
-            for (int j = (int)(center.getX() - radiusDouble); j < (center.getX()+radiusDouble+1) ; j++) {
+        if(isInteger((float) center.getX()) && isInteger((float) center.getY())){
+            points.put(new Point2D(center.getX(),center.getY()),0d);
+        }
+        for (float i = (float) (center.getY() + floatRadius); i > (center.getY() - floatRadius - 1) ; i--) {
+            if(!isInteger(i)){
+                continue;
+            }
+            for (float j = (float) (center.getX() - floatRadius); j < (center.getX()+floatRadius+1) ; j++) {
+                if(!isInteger(j)){
+                    continue;
+                }
                 Point2D point = new Point2D(j,i);
+                if(!points.isEmpty() && points.firstKey().equals(point)){
+                    continue;
+                }
                 BigDecimal distance =new BigDecimal(Task15.distance(point,center));
                 distance = distance.setScale(4, BigDecimal.ROUND_DOWN);
                 point.setDistance(distance.doubleValue());
                 points.put(point,distance.doubleValue());
             }
         }
+
         try{
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(output));
             objectOutputStream.write(points.size());
@@ -52,6 +65,10 @@ public class Task16 implements ITestableTask16 {
             e.printStackTrace();
         }
         return file;
+    }
+    private boolean isInteger(float n){
+        int a = (int) n;
+        return (n - a) == 0;
     }
     /**
      * Представляет файл, содержащий информацию о найденных точках.
