37674ed020431e9062b6e7697bc8b886cbad482e
nzenkova <natalka22-96@hotmail.com>
2017-11-27 00:21:03 +0300

    Task 2.19

--- src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask19.java
+++ src/main/java/com/epam/courses/jf/practice/nzenkova/second/TestableTask19.java
@@ -11,7 +11,7 @@ public class TestableTask19 implements ITestableTask19 {
         int numberOvertaking = 0;
         for (ICar car : cars) {
             for (ICar iCar : cars) {
-                //для всех пар, у которых car1.speed > car2.speed
+                //for all pairs: car1.speed > car2.speed
                 numberOvertaking += getNumber(car, iCar, numberLaps, lengthLap);
             }
         }
@@ -19,26 +19,26 @@ public class TestableTask19 implements ITestableTask19 {
     }
 
     /**
-     * @param car1 первая машина
-     * @param car2 вторая машина
-     * @return Число обгонов
+     * @param car1 The first car.
+     * @param car2 The second car.
+     * @return Number of overtaking.
      */
     private int getNumber(ICar car1, ICar car2, int laps, long lengthLap) {
         int number = 0;
-        //скорость сближения
+        //the approach velocity
         double speed = car1.getSpeed() - car2.getSpeed();
         if (Double.compare(speed,0) > 0) {
-            //расстояние от 1 до 2 машины
+            //the distance from 1 to 2 cars
             double pos = car2.getStartPosition() - car1.getStartPosition();
             while(Double.compare(pos, 0d) < 0) pos += lengthLap;
-            // время до встречи с начальных позиций
+            //the time to meet with the starting positions
             double time1 = pos / speed;
             //if(Double.compare(time1, 0) == 0) number--;
-            //место первой встречи
+            //the first meeting place
             double position = car1.getStartPosition() + time1 * car1.getSpeed();
             double time2 = (double)lengthLap / speed;
             while (Double.compare(position, lengthLap * laps) < 0) {
-                number++;
+                ++number;
                 position += time2 * car1.getSpeed();
             }
         }
