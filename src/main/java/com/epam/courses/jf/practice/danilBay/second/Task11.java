9d9e74027633df998a0cce7d59d45022933fdb0e
dana <danilikus17@gmail.com>
2017-11-26 18:23:37 +0300

    19

--- src/main/java/com/epam/courses/jf/practice/danilBay/second/Task11.java
+++ src/main/java/com/epam/courses/jf/practice/danilBay/second/Task11.java
@@ -9,6 +9,7 @@ public class Task11 implements ITestableTask11 {
     @Override
     public String emulate(ArrayList<String> peoples) {
         for(int i=0; peoples.size()>1; i+=2){
+
             peoples.remove(i%peoples.size());
         }
         return peoples.get(0);
@@ -20,5 +21,15 @@ public class Task11 implements ITestableTask11 {
             peoples.remove(i%peoples.size());
         }
         return peoples.get(0);
+    }
+
+    public static void main(String[] args) {
+        ArrayList <Integer>d =new ArrayList(15);
+        d.add(2);
+        d.add(6);
+        d.add(5);
+        System.out.println(d.size());
+        d.remove(1);
+        System.out.println(d.size());
     }
 }
--- src/main/java/com/epam/courses/jf/practice/danilBay/second/Task14.java
+++ src/main/java/com/epam/courses/jf/practice/danilBay/second/Task14.java
@@ -1,10 +1,114 @@
 package com.epam.courses.jf.practice.danilBay.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask14;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ArrayList;
 public class Task14 implements ITestableTask14 {
+    class Col<T extends Number> implements INumberCollection {
+        ArrayList<T>c;
+        Col(){
+            c=new ArrayList<>();
+        }
+        @Override
+        public Number nearest(Number value) {
+            double near=Math.abs(c.get(0).doubleValue()-value.doubleValue());
+            int res=0;
+            double buf;
+            for( int i=1; i<c.size(); i++){
+                buf=Math.abs(c.get(i).doubleValue()-value.doubleValue());
+                if(buf<near){
+                    res=i;
+                    near=buf;
+                }
+            }
+            return c.get(res);
+
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+
+            if(c.contains((T)o))return true;
+            else return false;
+        }
+
+        @NotNull
+        @Override
+        public Iterator iterator() {
+            return null;
+        }
+
+        @NotNull
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public boolean add(Object  o) {
+            add((T)o);
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            c.remove((T)o);
+            return false;
+        }
+
+        @Override
+        public boolean addAll(@NotNull Collection c) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public boolean retainAll(@NotNull Collection c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(@NotNull Collection c) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(@NotNull Collection c) {
+            return false;
+        }
+
+        @NotNull
+        @Override
+        public Object[] toArray(@NotNull Object[] a) {
+            return new Object[0];
+        }
+    }
     @Override
     public <T extends Number> INumberCollection<T> createCollection(Class<T> required) {
-        return null;
+
+        return new Col<T>();
+    }
+
+    public static void main(String[] args) {
+        Task14 d=new Task14();
+
+        //Col<Integer> s=d.createCollection(Integer.TYPE);
     }
 }
