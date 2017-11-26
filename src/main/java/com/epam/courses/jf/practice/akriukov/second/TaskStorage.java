87ccbc007413c55c3922d8c10d907f4f1d59cb5d
AleksandrKriukov <alexanderkry@gmail.com>
2017-11-26 19:58:15 +0300

    task2.15 done

--- src/main/java/com/epam/courses/jf/practice/akriukov/second/TaskStorage.java
+++ src/main/java/com/epam/courses/jf/practice/akriukov/second/TaskStorage.java
@@ -49,10 +49,10 @@ public class TaskStorage implements ITaskStorage {
         if (taskInterface == ITestableTask14.class){
             return (T) new Task14();
         }
-        /*if (taskInterface == ITestableTask15.class){
+        if (taskInterface == ITestableTask15.class){
             return (T) new Task15();
         }
-        if (taskInterface == ITestableTask16.class){
+        /*if (taskInterface == ITestableTask16.class){
             return (T) new Task16();
         }
         if (taskInterface == ITestableTask17.class){
