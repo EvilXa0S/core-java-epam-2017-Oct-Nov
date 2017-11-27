ac6fbcc12e63a84406d7b94470c2e24c58af5759
vakhonin <isvakhonin@yandex.ru>
2017-11-27 12:42:42 +0300

    rewrite_2part_11-12

--- src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task12.java
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task12.java
@@ -7,19 +7,25 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-/**
- * Created by igorvahonin on 13.11.17.
- */
 public class Task12 implements ITestableTask12{
 
     @Override
-    public List<Integer> transform(List<Integer> integers, int value) {
-        Set<Integer> set = new TreeSet<>(integers);
+//    public List<Integer> transform(List<Integer> integers, int value) {
+//        Set<Integer> set = new TreeSet<>(integers);
+//        List<Integer> list = new ArrayList<>(set);
+//
+//        return list;
+//    }
 
-        List<Integer> list = new ArrayList<>(set);
+    public List<Integer> transform(List<Integer> integers, int value){
+        for(int j = integers.size()-1; j > 0; j--) {
+            if(integers.get(j) <= value) {
+                integers.set(j, integers.get(j) ^ integers.get(0));
+                integers.set(0, integers.get(j) ^ integers.get(0));
+                integers.set(j, integers.get(j) ^ integers.get(0));
+            }
+        }
 
-
-
-        return list;
+        return integers;
     }
 }
