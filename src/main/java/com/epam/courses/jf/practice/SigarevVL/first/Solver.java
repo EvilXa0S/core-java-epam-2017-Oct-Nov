618b989d0be6015c6f9806c60611e926edd832c9
SigarevVL <SigarevVL94@gmail.com>
2017-11-27 23:22:59 +0300

    add javadoc

--- src/main/java/com/epam/courses/jf/practice/SigarevVL/first/Solver.java
+++ src/main/java/com/epam/courses/jf/practice/SigarevVL/first/Solver.java
@@ -1245,7 +1245,7 @@ public class Solver implements ISolver{
         System.out.println(matrix.length);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++){
-                System.out.print(j + "\t");
+                System.out.print(matrix[i][j] + "\t");
             }
             System.out.println();
         }
