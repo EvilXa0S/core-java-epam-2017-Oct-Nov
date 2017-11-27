3bba864faeed5500e5646ae7848a465c062b9bc0
vakhonin <isvakhonin@yandex.ru>
2017-11-27 13:37:06 +0300

    rewrite_only_task2_12

--- src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task12.java
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task12.java
@@ -2,41 +2,25 @@ package com.epam.courses.jf.practice.vakhonin.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask12;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
-public class Task12 implements ITestableTask12{
+public class Task12 implements ITestableTask12 {
+
+    //        integers.sort(Comparator.naturalOrder()); // Maybe this method (in one line) also fits to do this task... :-)
 
     @Override
-//    public List<Integer> transform(List<Integer> integers, int value) {
-//        Set<Integer> set = new TreeSet<>(integers);
-//        List<Integer> list = new ArrayList<>(set);
-//
-//        return list;
-//    }
-
-    public List<Integer> transform(List<Integer> integers, int value){
+    public List<Integer> transform(List<Integer> integers, int value) {
         int k = 0;
-        for(int j = integers.size()-1; j > k; j--) {
-            while((integers.get(j) <= value) && (j>k)){
-//                System.out.println("integers[j] = " + integers.get(j));
-//                System.out.println("j = " + j + "  k = " + k);
-//                System.out.println(integers);
+        for (int j = integers.size() - 1; j > k; j--) {
+            while ((integers.get(j) <= value) && (j > k)) {
                 integers.set(j, integers.get(j) ^ integers.get(k));
                 integers.set(k, integers.get(j) ^ integers.get(k));
                 integers.set(j, integers.get(j) ^ integers.get(k));
-//                System.out.println(integers);
                 k++;
             }
-            k--;
-//            if(j==k){
-//                break;
-//            }
-        }
 
-//        integers.sort(Comparator.naturalOrder()); // Maybe this method (in one line) also fits to do this task... :-)
+            k--;
+        }
 
         return integers;
     }
