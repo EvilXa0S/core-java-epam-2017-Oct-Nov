a9a9c06a9a750538065497f3a16bcc286c36ce36
nbikbaev <niaz.bikbaev@gmail.com>
2017-11-27 19:42:08 +0300

    RectangularIntegerMatrix extracted to separate file

---
+++ src/main/java/com/epam/courses/jf/practice/nbikbaev/second/RectangularIntegerMatrix.java
@@ -0,0 +1,53 @@
+package com.epam.courses.jf.practice.nbikbaev.second;
+
+import com.epam.courses.jf.practice.common.second.ITestableTask18.IRectangularIntegerMatrix;
+
+import java.util.Arrays;
+
+public class RectangularIntegerMatrix implements IRectangularIntegerMatrix {
+    private int[][] a;
+    private int width;
+    private int height;
+
+
+    public RectangularIntegerMatrix(int[][] a) {
+        this.a = a;
+        width = a[0].length;
+        height = a.length;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public int getValue(int indexWidth, int indexHeight) {
+        return a[indexHeight][indexWidth];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RectangularIntegerMatrix that = (RectangularIntegerMatrix) o;
+
+        if (width != that.width) return false;
+        if (height != that.height) return false;
+        return Arrays.deepEquals(a, that.a);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.deepHashCode(a);
+        result = 31 * result + width;
+        result = 31 * result + height;
+        return result;
+    }
+}
