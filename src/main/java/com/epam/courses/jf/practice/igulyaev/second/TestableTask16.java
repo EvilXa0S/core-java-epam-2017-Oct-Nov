6ee03fae181b83734724c03e2eb3f79e0641d8ef
gulyaev13 <gulyaev13@gmail.com>
2017-11-30 01:34:57 +0300

    Task 2.16 updated: solve problem with Serialization

--- src/main/java/com/epam/courses/jf/practice/igulyaev/second/TestableTask16.java
+++ src/main/java/com/epam/courses/jf/practice/igulyaev/second/TestableTask16.java
@@ -15,8 +15,8 @@ public class TestableTask16 implements ITestableTask16 {
             double r2 = Math.pow(p2.getX() - center.getX(), 2) + Math.pow(p2.getY() - center.getY(), 2);
             return Double.compare(r1, r2);
         });
-        for(long x = (long)(center.getX() - radius); x < Math.ceil(center.getX() - radius); ++x){
-            for(long y = (long)(center.getY() - radius); y < Math.ceil(center.getY() - radius); ++y){
+        for(long x = (long)(center.getX() - radius); x < Math.ceil(center.getX() + radius); ++x){
+            for(long y = (long)(center.getY() - radius); y < Math.ceil(center.getY() + radius); ++y){
                 double distance = Math.sqrt(Math.pow(x - center.getX(), 2) + Math.pow(y - center.getY(), 2));
                 if(distance < radius){
                     points.put(new Point2D(x, y), distance);
@@ -25,9 +25,10 @@ public class TestableTask16 implements ITestableTask16 {
         }
         try(FileOutputStream fileOutputStream = new FileOutputStream(output);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)){
-            points.entrySet().forEach(obj -> {
+            points.forEach((key, value) -> {
                 try {
-                    objectOutputStream.writeObject(obj);
+                    objectOutputStream.writeObject(key);
+                    objectOutputStream.writeDouble(value);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -46,8 +47,6 @@ public class TestableTask16 implements ITestableTask16 {
 
          FileWithPoints(File file, Comparator<I2DPoint> comparator) {
             this.file = file;
-
-
             this.comparator = comparator;
         }
 
@@ -61,14 +60,33 @@ public class TestableTask16 implements ITestableTask16 {
                 try(FileInputStream fileInputStream = new FileInputStream(file);
                 ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)){
                     Map.Entry<I2DPoint, Double> entry;
-                    while ((entry = (Map.Entry<I2DPoint, Double>) objectInputStream.readObject()) != null){
-                        points.put(entry.getKey(), entry.getValue());
+                    while (true){
+                        I2DPoint key = (I2DPoint) objectInputStream.readObject();
+                        Double value = objectInputStream.readDouble();
+                        points.put(key,value);
                     }
-                } catch (IOException | ClassNotFoundException e) {
+                }catch (EOFException e){
 
+                } catch (IOException | ClassNotFoundException e) {
+                    e.fillInStackTrace();
                 }
             }
             return points;
         }
-   }
+    }
+
+    public static void main(String[] args) {
+        TestableTask16 tt = new TestableTask16();
+        IFileWithPoints fileWithPoints = tt.analyze(new Point2D(0, 0), 1, prepareFile());
+        SortedMap<I2DPoint, Double> points = fileWithPoints.getPoints();
+        points.forEach((key, value) -> System.out.println(key.getX() + " " + key.getY() + " : " + value));
+    }
+    static protected File prepareFile() {
+        try {
+            File tmpFile = File.createTempFile("2-epam-spring-2016", ".tmp", new File("C:\\Users\\Таня\\Java\\EPAM"));
+            return tmpFile;
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
