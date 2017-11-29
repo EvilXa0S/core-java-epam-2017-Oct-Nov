e85f4123290b374cb9c33d0bc4c62c38a1cb73d6
vakhonin <isvakhonin@yandex.ru>
2017-11-29 11:58:16 +0300

    rewrite_2_11

--- src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task11.java
+++ src/main/java/com/epam/courses/jf/practice/vakhonin/second/Task11.java
@@ -6,36 +6,24 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-///**
-// * В кругу стоят N человек.
-// * На каждой итерации цикла выбывает человек (через одного, начиная с первого), пока не останется единственный.
-// * Разработать два решения, моделирующие процесс.
-// * Первое должно использовать класс ArrayList, а второе – LinkedList.
-// */
-
-public class Task11 implements ITestableTask11 {
-
-//    /**
-//     * Выполняет эмуляцию поставленной задачи.
-//     * @param peoples Список с именами участников.
-//     * @return Имя последнего оставшегося.
-//     */
+public class Task11 implements ITestableTask11{
 
     @Override
     public String emulate(ArrayList<String> peoples){
+
+
         Iterator<String> it;
         int shift = 0;
-
         while(peoples.size() != 1){
             it = peoples.iterator();
-
             if(shift == 1){
                 it.next();
                 shift--;
             }
-
             while(it.hasNext()){
                 if(shift == 0){
+                    System.out.println(it.next());
+
                     it.remove();
                     shift++;
                 }
@@ -45,31 +33,23 @@ public class Task11 implements ITestableTask11 {
                 }
             }
         }
-
         return peoples.get(0);
     }
-
-//    /**
-//     * Выполняет эмуляцию поставленной задачи.
-//     * @param peoples Список с именами участников.
-//     * @return Имя последнего оставшегося.
-//     */
 
     @Override
     public String emulate(LinkedList<String> peoples){
         Iterator<String> it;
         int shift = 0;
-
         while(peoples.size() != 1){
             it = peoples.iterator();
-
             if(shift == 1){
                 it.next();
                 shift--;
             }
-
             while(it.hasNext()){
                 if(shift == 0){
+                    System.out.println(it.next());
+
                     it.remove();
                     shift++;
                 }
@@ -79,7 +59,7 @@ public class Task11 implements ITestableTask11 {
                 }
             }
         }
-
         return peoples.get(0);
     }
+
 }
