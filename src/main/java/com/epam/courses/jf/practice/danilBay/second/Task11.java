9b8e8776c58b23d3c7c99f1f753381ff66679739
dana <danilikus17@gmail.com>
2017-12-03 21:32:10 +0300

    19

--- src/main/java/com/epam/courses/jf/practice/danilBay/second/Task11.java
+++ src/main/java/com/epam/courses/jf/practice/danilBay/second/Task11.java
@@ -3,6 +3,7 @@ package com.epam.courses.jf.practice.danilBay.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask11;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Task11 implements ITestableTask11 {
@@ -17,12 +18,24 @@ public class Task11 implements ITestableTask11 {
 
     @Override
     public String emulate(LinkedList<String> peoples) {
-        for(int i=0; peoples.size()>1; i=(i++)%peoples.size()){
-            peoples.remove(i%peoples.size());
+        Iterator iter = peoples.iterator();
+
+        int out = 1;
+        while (peoples.size() > 1) {
+            if (iter.hasNext()) {
+                ++out;
+                iter.next();
+                if (out % 2 == 0) {
+                    iter.remove();
+                    out = 0;
+                }
+
+            } else {
+                iter = peoples.iterator();
+            }
         }
         return peoples.get(0);
     }
-
     public static void main(String[] args) {
         ArrayList <Integer>d =new ArrayList(15);
         d.add(2);
