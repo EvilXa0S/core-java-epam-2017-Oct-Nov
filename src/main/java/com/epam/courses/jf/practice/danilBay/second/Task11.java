cbcb9b0cc06540e8159dc78ea55d709cf12f924b
dana <danilikus17@gmail.com>
2017-12-11 14:07:01 +0300

    19

--- src/main/java/com/epam/courses/jf/practice/danilBay/second/Task11.java
+++ src/main/java/com/epam/courses/jf/practice/danilBay/second/Task11.java
@@ -4,11 +4,12 @@ import com.epam.courses.jf.practice.common.second.ITestableTask11;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Task11 implements ITestableTask11 {
     @Override
     public String emulate(ArrayList<String> peoples) {
-        for(int i=0; peoples.size()>1; i=(i++)%peoples.size()){
+        for(int i=0; peoples.size()>1; i=(i+1)%peoples.size()){
 
             peoples.remove(i);
         }
@@ -17,19 +18,23 @@ public class Task11 implements ITestableTask11 {
 
     @Override
     public String emulate(LinkedList<String> peoples) {
-        for(int i=0; peoples.size()>1; i=(i++)%peoples.size()){
+        for(int i=0; peoples.size()>1; i=(i+1)%peoples.size()){
             peoples.remove(i);
         }
         return peoples.get(0);
     }
 
     public static void main(String[] args) {
-        ArrayList <Integer>d =new ArrayList(15);
-        d.add(2);
-        d.add(6);
-        d.add(5);
-        System.out.println(d.size());
-        d.remove(1);
-        System.out.println(d.size());
+
+
+        Task11 tsk11=new Task11();
+        LinkedList<String> d =new LinkedList<String>();
+        d.add(2+"");
+        d.add(6+"");
+        d.add(5+"");
+        d.add(8+"");
+        d.add(9+"");
+
+        System.out.println(tsk11.emulate(d));
     }
 }
