b22e7238f0cb361f8fead76f1c90d6a223eb202b
dana <danilikus17@gmail.com>
2017-12-22 16:06:08 +0300

    19

--- src/main/java/com/epam/courses/jf/practice/danilBay/second/Task10.java
+++ src/main/java/com/epam/courses/jf/practice/danilBay/second/Task10.java
@@ -2,9 +2,7 @@ package com.epam.courses.jf.practice.danilBay.second;
 
 import com.epam.courses.jf.practice.common.second.ITestableTask10;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -14,24 +12,22 @@ public class Task10 implements ITestableTask10 {
     @Override
     public HashMap<String, Integer> countNumberWords(File input) {
         HashMap<String,Integer> res=new HashMap<>();
-        String temp;
+        String temp,line;
         Integer integer;
-        try (Scanner fileScanner = new Scanner((new FileReader(input)))) {
-            fileScanner.useDelimiter(" ");
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(input))) {
+            while (( line = bufferedReader.readLine()) != null) {
+                for (String buf : line.split(" ")) {
 
-            while (fileScanner.hasNext()){
-                temp=fileScanner.next();
-
-                if(res.containsKey(temp)){
-                    integer=res.get(temp)+1;
+                    if(res.containsKey(buf)){
+                        integer=res.get(buf)+1;
+                    }
+                    else {
+                        integer=1;
+                    }
+                    res.put(buf,integer);
                 }
-                else {
-                    integer=1;
-                }
-                res.put(temp,integer);
             }
-            fileScanner.close();
-        } catch (FileNotFoundException e) {
+        }   catch (IOException e) {
             e.printStackTrace();
         }
 
--- src/main/java/com/epam/courses/jf/practice/danilBay/second/Task11.java
+++ src/main/java/com/epam/courses/jf/practice/danilBay/second/Task11.java
@@ -3,6 +3,7 @@ package com.epam.courses.jf.practice.danilBay.second;
 import com.epam.courses.jf.practice.common.second.ITestableTask11;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,10 +19,23 @@ public class Task11 implements ITestableTask11 {
 
     @Override
     public String emulate(LinkedList<String> peoples) {
-        for(int i=0; peoples.size()>1; i=(i+1)%peoples.size()){
-            peoples.remove(i);
+        int counter = 0;
+        while(peoples.size() != 1) {
+            Iterator<String> it = peoples.iterator();
+            while (it.hasNext()) {
+                String s = it.next();
+                if (counter % 2 == 0)
+                    it.remove();
+                counter++;
+            }
         }
         return peoples.get(0);
     }
+//    public String emulate(LinkedList<String> peoples) {
+//        for(int i=0; peoples.size()>1; i=(i+1)%peoples.size()){
+//            peoples.remove(i);
+//        }
+//        return peoples.get(0);
+//    }
 
 }
